39efc7eb2bc75cca13e6964ac44b9be6e8d5625a
VengAnn <venganncoco@gmail.com>
2023-07-24 14:32:15 +0700

    update

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'Page/CategoryPage.dart';
+import 'Page/homepage.dart';
+
+void main() {
+  runApp(const Home());
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    //to hint status bar on below button
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 31, 31, 33),
+      ),
+      routes: {
+        "/": (context) => HomePage(),
+        "CategoryPage": (context) => CategoryPage(),
+      },
+    );
+  }
+}
